38bfd197003a8a4a02dee2ecf401fc5d6be46763
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-04-03 21:04:13 -0400

    code update to our DB settings and Twig version

--- App/Config.php
+++ App/Config.php
@@ -32,11 +32,11 @@ class Config
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'secret';
+    const DB_PASSWORD = '';
 
     /**
      * Show or hide error messages on screen
      * @var boolean
      */
-    const SHOW_ERRORS = false;
+    const SHOW_ERRORS = true;
 }
